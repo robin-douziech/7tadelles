d2f6d330feff69b18d898be9cca19637b7fb692e
robin-douziech <robin.douziech@etu.esisar.grenoble-inp.fr>
2023-09-13 11:27:51 +0200

    fix bug

--- sept_tadelles/game_calendar/views.py
+++ sept_tadelles/game_calendar/views.py
@@ -1,6 +1,6 @@
 from django.contrib import admin as django_admin
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from account import admin as account_admin
