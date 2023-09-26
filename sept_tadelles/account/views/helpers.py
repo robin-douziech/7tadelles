e6cb3f18ad9ae7f8abe39c5f9965a069696df7a2
robin-douziech <robin.douziech@etu.esisar.grenoble-inp.fr>
2023-09-26 22:20:17 +0200

    v4.2.0

--- sept_tadelles/account/views/helpers.py
+++ sept_tadelles/account/views/helpers.py
@@ -107,6 +107,7 @@ def clean_user(user) :
 						post_args = None,
 						created_at = dt.datetime.now()
 					)
+					notifications.save()
 					send_notification(notification, [user])
 
 def get_actions(request) :
